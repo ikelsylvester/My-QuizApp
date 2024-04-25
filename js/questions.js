7210fa39d42e8c5ca08dcac49cf1617dadf17950
Ikel Sylvester jr <167588167+088020634@users.noreply.github.com>
2024-04-25 16:46:45 +0000

    initialcommit

---
+++ js/questions.js
@@ -0,0 +1,132 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+
+  {
+    numb: 6,
+    question: "Which of the following is not a JavaScript built-in function?",
+    answer: "liberia()",
+    options: [
+      "getElementById()",
+      "addEventListener()",
+      "SetInterval()",
+      "liberia()",
+    ]
+  },
+
+  {
+    numb: 7,
+    question: "Which of the following operator are use to do addition and assignment?",
+    answer: "+=",
+    options: [
+      ">=",
+      "+=",
+      "*=",
+      "<>"
+    ]
+  },
+  
+  {
+    numb: 8,
+    question: "In JavaScript to check if two values are the same we use the following operator.",
+    answer: "==",
+    options: [
+      "==",
+      "!=",
+      "??",
+      "%="
+    ]
+  },
+
+  {
+    numb: 9,
+    question: "Which of the following data types store it's value in indexes?",
+    answer: "Array",
+    options: [
+      "object",
+      "Strings",
+      "Array",
+      "None",
+    ]
+  },
+
+  {
+    numb: 10,
+    question: "The proper way of writting an 'if'statement is?",
+    answer: "if(){}",
+    options: [
+      "if<>",
+      "None",
+      "if()",
+      "if(){}"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
